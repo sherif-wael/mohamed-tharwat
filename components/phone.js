75e0e847f1a4181f72c8187b52eecfb173b226d0
Sherif Wael <sherifwael80@gmail.com>
2020-07-16 21:01:35 +0200

    initial commit

--- components/phone.js
+++ components/phone.js
@@ -1,20 +1,24 @@
 import styled from "styled-components";
-
+import media from "../styles/media";
 
 
 const StyledContainer = styled.a`
     background-color: ${props => props.theme.colors.secondary};
     box-shadow: 0 8px 8px -5px rgb(0, 0, 0, 0.3);
     border-radius: 5px;
+    padding: 10px 10px 6px;
     img{
         width: 30px;
         height: 30px;
-        padding: 10px;
     }
     position: fixed;
     right: 30px;
     bottom: 30px;
     z-index: 4;
+    ${media(600)`
+        right: 20px;
+        bottom: 20px;
+    `}
 `
 
 function Phone(){
