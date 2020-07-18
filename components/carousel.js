93e3e0ed4332b59f2c0ac855ae21ad5029a205c0
Sherif Wael <sherifwael80@gmail.com>
2020-07-18 08:15:16 +0200

    initial commit

---
+++ components/carousel.js
@@ -0,0 +1,40 @@
+import Flickity from "react-flickity-component";
+import styled from "styled-components";
+import media from "../styles/media";
+
+const StyledContainer = styled.div`
+    margin-bottom: 30px;
+    .carousel{
+        height: 500px;
+        width: 800px;
+        margin: 0 auto;
+        ${media(600)`
+            height: 400px;
+        `}
+        img{
+            width: 100%;
+            height: 100%;
+        }
+    }
+`
+
+function Carousel(){
+    return (
+        <StyledContainer>
+            <Flickity
+            className="carousel"
+            elementType="div"
+            disableImagesLoaded={false}
+            reloadOnUpdate
+            static
+            >
+                <img src="/images/clinic1.jpeg" alt="clinic image" />
+                <img src="/images/clinic2.jpeg" alt="clinic image" />
+                <img src="/images/clinic3.jpeg" alt="clinic image" />
+                <img src="/images/clinic4.jpeg" alt="clinic image" />
+            </Flickity>
+        </StyledContainer>
+    )
+}
+
+export default Carousel;
--- pages/clinic.js
+++ pages/clinic.js
@@ -3,24 +3,21 @@ import styled from "styled-components";
 import Map from "../components/map";
 import Schedule from  "../components/schedule";
 import Register from "../components/register";
-import Slider from "../components/slider";
+import Carousel from "../components/carousel";
+
+
 
 const StyledMapWrapper = styled.div`
     height: 400px;
     position: relative;
 `
 
-const imgs = [
-    "/images/clinic1.jpeg",
-    "/images/clinic2.jpeg",
-    "/images/clinic3.jpeg",
-    "/images/clinic4.jpeg"
-]
+
 
 export default function Clinic(){
     return (
         <Layout>
-            <Slider imgs={imgs} />
+            <Carousel />
             {/* <StyledMapWrapper>
                 <Map />
             </StyledMapWrapper> */}
