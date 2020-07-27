9ebf251a845479cd234dcc710f9c2de1622a9d3f
Sherif Wael <sherifwael80@gmail.com>
2020-07-27 22:15:32 +0200

    initial commit

--- components/layout.js
+++ components/layout.js
@@ -9,8 +9,8 @@ function Layout({children}){
     return (
         <div>
             <Head>
-                <meta name="description" content="دكتور محمد ثروت حجازى Dr. Mohamed Thrawat Hegazy، دكتوراه الباطنة العامة و الروماتيزم و المناعة، مدرس الباطنة العامة و الروماتيزم و المناعة و التهابات الاوعية الدموية كلية طب القصر العينى" />
-                <title>Dr. Mohamed Tharwat</title>
+                <meta name="description" content="دكتور محمد ثروت حجازى، دكتوراه الباطنة العامة و الروماتيزم و المناعة، مدرس الباطنة العامة و الروماتيزم و المناعة و التهابات الاوعية الدموية كلية طب القصر العينى" />
+                <title>Dr. Mohamed Tharwat Hegazy</title>
                 <link href="https://fonts.googleapis.com/css2?family=Markazi+Text&family=Tajawal&display=swap" rel="stylesheet"></link>
             </Head>
             <GlobalStyles />
