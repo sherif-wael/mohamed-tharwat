18a2aea03fb2b8d55bffc4640ae9248c2b9bc4e6
Sherif Wael <sherifwael80@gmail.com>
2020-07-30 01:48:34 +0200

    initial commit

--- components/layout.js
+++ components/layout.js
@@ -12,6 +12,7 @@ function Layout({children}){
                 <meta name="description" content="دكتور محمد ثروت حجازى، دكتوراه الباطنة العامة و الروماتيزم و المناعة، مدرس الباطنة العامة و الروماتيزم و المناعة و التهابات الاوعية الدموية كلية طب القصر العينى" />
                 <title>Dr. Mohamed Tharwat Hegazy</title>
                 <link href="https://fonts.googleapis.com/css2?family=Markazi+Text&family=Tajawal&display=swap" rel="stylesheet"></link>
+                <meta name="google-site-verification" content="WmwMDA3tjh9lzqIR6OAkV841bInIcmfG1Ouw56Vbbfw" />
             </Head>
             <GlobalStyles />
             <Phone />
