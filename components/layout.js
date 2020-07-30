aca4e9aa00f0fdaaf975e268d043606dcb4a94ed
Sherif Wael <sherifwael80@gmail.com>
2020-07-30 02:11:00 +0200

    initial commit

--- components/layout.js
+++ components/layout.js
@@ -13,6 +13,20 @@ function Layout({children}){
                 <title>Dr. Mohamed Tharwat Hegazy</title>
                 <link href="https://fonts.googleapis.com/css2?family=Markazi+Text&family=Tajawal&display=swap" rel="stylesheet"></link>
                 <meta name="google-site-verification" content="WmwMDA3tjh9lzqIR6OAkV841bInIcmfG1Ouw56Vbbfw" />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `<!-- Global site tag (gtag.js) - Google Analytics -->
+                        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-173947665-1"></script>
+                        <script>
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                        
+                            gtag('config', 'UA-173947665-1');
+                        </script>
+                        `,
+                    }}
+                />
             </Head>
             <GlobalStyles />
             <Phone />
