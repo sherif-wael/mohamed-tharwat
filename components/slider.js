dd6c2cb2b7036a4ac5db50349443a4372addb420
MohamedWael1 <mohwael00@gmail.com>
2024-03-14 21:34:13 +0200

    add new clinic images

--- components/slider.js
+++ components/slider.js
@@ -1,70 +1,76 @@
 import Carousel from "nuka-carousel";
 import styled from "styled-components";
-import media from "../styles/media"
+import media from "../styles/media";
 
 const StyledContainer = styled.div`
-    .carousel{
-        .dots{
-            width: 36px;
-            height: 36px;
-            &:focus, &:active{
-                outline: none;
-            }
-            svg{
-                width: 12px;
-                height: 12px;
-                circle{
-                    fill: rgb(255, 255, 255, 0.9);
-                    cx: 5;
-                    cy: 5;
-                    r: 5
-                }
-            }
+  .carousel {
+    .dots {
+      width: 36px;
+      height: 36px;
+      &:focus,
+      &:active {
+        outline: none;
+      }
+      svg {
+        width: 12px;
+        height: 12px;
+        circle {
+          fill: rgb(255, 255, 255, 0.9);
+          cx: 5;
+          cy: 5;
+          r: 5;
         }
-        .btn{
-            border: none;
-            background-color: transparent;
-            cursor: pointer;
-            &:focus, &:active{
-                outline: none;
-            }
-            img{
-                width: 30px;
-                height: 30px;         
-            }
-        }
+      }
     }
-`
-
+    .btn {
+      border: none;
+      background-color: transparent;
+      cursor: pointer;
+      &:focus,
+      &:active {
+        outline: none;
+      }
+      img {
+        width: 30px;
+        height: 30px;
+      }
+    }
+  }
+`;
 
 let defaultControlsConfig = {
-    containerClassName: "carousel",
-    pagingDotsClassName: "dots" 
-}
+  containerClassName: "carousel",
+  pagingDotsClassName: "dots",
+};
 
-
-
-function Slider({images}){
-    let prevButton = ({previousSlide}) => {
-        return <button onClick={previousSlide} className="btn"><img src="/images/return.svg" alt="return img" /></button>
-    }
-    let nextButton = ({nextSlide}) => {
-        return <button onClick={nextSlide} className="btn"><img src="/images/next.svg" alt="next img" /></button>
-    }
+function Slider({ images }) {
+  let prevButton = ({ previousSlide }) => {
     return (
-        <StyledContainer>
-            <Carousel defaultControlsConfig={defaultControlsConfig} 
-                renderCenterLeftControls={prevButton}
-                renderCenterRightControls={nextButton}
-                >
-                {
-                    images.map((img, i) => (
-                        <img src={img} alt="slide image" key={i} className="slide" />
-                    ))
-                }
-            </Carousel>
-        </StyledContainer>
-    )
+      <button onClick={previousSlide} className="btn">
+        <img src="/images/return.svg" alt="return img" />
+      </button>
+    );
+  };
+  let nextButton = ({ nextSlide }) => {
+    return (
+      <button onClick={nextSlide} className="btn">
+        <img src="/images/next.svg" alt="next img" />
+      </button>
+    );
+  };
+  return (
+    <StyledContainer>
+      <Carousel
+        defaultControlsConfig={defaultControlsConfig}
+        renderCenterLeftControls={prevButton}
+        renderCenterRightControls={nextButton}
+      >
+        {images.map((img, i) => (
+          <img src={img} alt="slide image" key={i} className="slide"/>
+        ))}
+      </Carousel>
+    </StyledContainer>
+  );
 }
 
-export default Slider
+export default Slider;
--- pages/clinic.js
+++ pages/clinic.js
@@ -1,27 +1,24 @@
 import Layout from "../components/layout";
 import styled from "styled-components";
 import Map from "../components/map";
-import Schedule from  "../components/schedule";
+import Schedule from "../components/schedule";
 import Register from "../components/register";
 import Carousel from "../components/clinic-carousel";
 
-
 const StyledMapWrapper = styled.div`
-    height: 400px;
-    position: relative;
-`
+  height: 400px;
+  position: relative;
+`;
 
-
-
-export default function Clinic(){
-    return (
-        <Layout>
-            <Carousel />
-            {/* <StyledMapWrapper>
+export default function Clinic() {
+  return (
+    <Layout>
+      <Carousel />
+      {/* <StyledMapWrapper>
                 <Map />
             </StyledMapWrapper> */}
-            <Schedule />
-            <Register />
-        </Layout>
-    )
+      <Schedule />
+      <Register />
+    </Layout>
+  );
 }
